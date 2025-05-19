38b23376954d52b5f306816757a5d00edf81a600
Zeshan-Malik <zeshanmalik5055@gmail.com>
2025-05-19 15:57:57 +0500

    Initial comment

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const CustomersPage = () => {
+
+    return (
+        <div className="p-6">
+            <h1 className="text-2xl font-bold mb-4">Customers</h1>
+        </div>
+    )
+}
+export default CustomersPage;
